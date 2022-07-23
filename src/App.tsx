cf4c3cf9038cb567fca20061208842eb29749fa3
Dereden399 <denisk399@gmail.com>
2022-07-23 17:57:54 +0300

    dependecies installed, main style set

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 function App() {
-  return <div>Hello! Initial commit</div>;
+  return (
+    <div className='p-10 bg-gradient-to-tr from-white to-pink-100 w-screen h-screen text-lg font-roboto font-normal'>
+      Hello! Initial commit
+    </div>
+  );
 }
 
 export default App;
